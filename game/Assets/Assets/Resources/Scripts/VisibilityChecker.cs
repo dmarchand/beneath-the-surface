8cb4143c02d854b478587e6970db4694c6d03524
Dan Marchand <dan.marchand@gmail.com>
2014-04-26 01:32:43 -0400

    Add spawn trigger flag so starting floors can’t force a shitload of floor spawns

--- game/Assets/Assets/Resources/Scripts/VisibilityChecker.cs
+++ game/Assets/Assets/Resources/Scripts/VisibilityChecker.cs
@@ -6,6 +6,7 @@ public class VisibilityChecker : MonoBehaviour {
 	Director _director;
 
 	public float ChunkWidth;
+	public bool TriggersSpawn;
 	float _movedDistance = 0f;
 	bool _spawned;
 
@@ -17,7 +18,7 @@ public class VisibilityChecker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!_spawned && _movedDistance >= ChunkWidth) {
+		if(TriggersSpawn && !_spawned && _movedDistance >= ChunkWidth) {
 			_director.SpawnSegment();
 			_spawned = true;
 		}
