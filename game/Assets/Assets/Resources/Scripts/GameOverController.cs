1c91bef33d7a7937689edb7ee125ad6110f3558f
Dan Marchand <dan.marchand@gmail.com>
2014-04-26 21:35:01 -0400

    Better key handling for game over

--- game/Assets/Assets/Resources/Scripts/GameOverController.cs
+++ game/Assets/Assets/Resources/Scripts/GameOverController.cs
@@ -32,7 +32,7 @@ public class GameOverController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.anyKeyDown)
+	    if (Input.GetKeyDown(KeyCode.Space))
         {
             Application.LoadLevel(1);
         }
